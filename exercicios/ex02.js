0fbbf8d890af7cd1acd95c44942760ddda0a28da
Inês Jobino <inesjobino336@gmail.com>
2025-10-27 15:32:47 +0100

    exercício sobre operadores

---
+++ exercicios/ex02.js
@@ -0,0 +1,12 @@
+//operadores
+let num1 = 1 ,numer2 = 2
+let num2 = 1
+let num3 = "1"
+const n = 20.5 
+let n2 = 4
+console.log(num1+num3)
+console.log(num1+num2)
+console.log(n2**numer2)
+console.log(n/n2)
+console.log(num1++)
+console.log(n2===n)
