c9b7b86543397972031a82471a4533bdc95b1712
Inês Jobino <inesjobino336@gmail.com>
2025-10-27 15:32:28 +0100

    exercício sobre variaveis

---
+++ exercicios/ex01.js
@@ -0,0 +1,16 @@
+//sobre variaveis
+let full_name = "Miss Sunshine" , occupation = "Student" ,contact = "+244 922 985 854"
+let age = 18
+let height = 1.60
+const religion = "Christian"
+const course = {name: "management informatics",subject: "programming" ,duration: 4}
+const persnalidade = ["introvert","shy","disciplined","confident","selfish","anxious"]
+let dating
+let crush
+
+//para saber qal é o tipo de uma variavel usamos a função typeof()
+console.log(typeof(full_name))
+
+//existem duas formas de concatenar variaveis a strings 
+console.log("I'm "+full_name+ " ,I'm "+age+" years old right now")
+console.log(`I'm ${religion} and I have ${height} cm`)
